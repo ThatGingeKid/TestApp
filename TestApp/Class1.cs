ff66eb64b7823337de773b4cfb5c9fdd72080e65
PDAJenkins <110927038+PDAJenkins@users.noreply.github.com>
2024-03-07 12:07:25 -0500

    Add project files.

---
+++ TestApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Calculator
+    {
+        private double valueOne { get; set; }
+        private double valueTwo { get; set; }
+
+        public double AddTest(double valueOne, double valueTwo)
+        {
+            return valueOne + valueTwo;
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCode = "";
+            while(exitCode != "exit")
+            {
+                Console.WriteLine("Hello World!");
+
+                Calculator newCalc = new Calculator();
+
+                double result = newCalc.AddTest(2.5, 3);
+                Console.WriteLine(result);
+                exitCode = Console.ReadLine();
+            }
+        }
+    }
+}
